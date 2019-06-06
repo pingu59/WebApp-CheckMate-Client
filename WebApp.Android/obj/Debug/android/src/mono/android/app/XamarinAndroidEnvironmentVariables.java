18ab99287b5fe1e17a64ed6497f03373a9863b35
pw2718 <PW2718@ic.ac.uk>
2019-06-06 17:54:23 +0100

    add media nuget package

--- WebApp.Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ WebApp.Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "aa9c3579-27ad-460f-b3e9-d9ee60497aaa",
+		"XAMARIN_BUILD_ID", "980f6783-c50f-4445-8d81-6b462e44bdf8",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
